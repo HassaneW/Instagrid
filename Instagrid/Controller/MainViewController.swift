1de1ab1a1b3a49e8e40a439b845a9cb0c413a659
HassaneW <wandianga33@gmail.com>
2020-01-26 13:31:03 +0000

    Instagrid

--- Instagrid/Controller/MainViewController.swift
+++ Instagrid/Controller/MainViewController.swift
@@ -11,38 +11,58 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK: - Initialization
-    @IBOutlet weak var swipeToShare: UILabel!
+    
+    @IBOutlet weak var swipeToShareLabel: UILabel!
     @IBOutlet weak var mainView : UIView!
+    
     //  Main view buttons
     @IBOutlet weak var buttonImageTopLeft: UIButton!
     @IBOutlet weak var buttonImageTopRight: UIButton!
     @IBOutlet weak var buttonImageBottomLeft: UIButton!
     @IBOutlet weak var buttonImageBottomRight: UIButton!
+    
     // Array of buttons
-    @IBOutlet var layoutButtons: [UIButton]!
+    @IBOutlet var buttonImageArray: [UIButton]!
+    
     // Button that allow us to select the view layout
     @IBOutlet weak var buttonActionLeft: UIButton!
     @IBOutlet weak var buttonActionCenter: UIButton!
     @IBOutlet weak var buttonActionRight: UIButton!
+    
     var buttonTapped : UIButton?
     var imagePicker = UIImagePickerController()
+    
     // Button that allow us to reset images
     var buttonTopLeftTapped = false
     var buttonTopRightTapped = false
     var buttonBottomLeftTapped = false
     var buttonBottomRightTapped = false
+    
     // MARK: - Make a swipe
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Swipe gesture left statement
-        let leftswipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare(_:)))
-        leftswipeGesture.direction = .left
-        self.view.addGestureRecognizer(leftswipeGesture)
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare))
+        leftSwipeGesture.direction = .left
+        self.view.addGestureRecognizer(leftSwipeGesture)
+        
         // Swipe gesture right statement
-        let TopswipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare(_:)))
-        TopswipeGesture.direction = .up
-        self.view.addGestureRecognizer(TopswipeGesture)
+        let topSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeToShare))
+        topSwipeGesture.direction = .up
+        self.view.addGestureRecognizer(topSwipeGesture)
+        
+        // Image ratio
+        for buttonImage in buttonImageArray {
+            buttonImage.imageView?.contentMode = .scaleAspectFill
+        }
+        
+        // Button selection default
+        buttonActionRight.isSelected = true
     }
+    
+    // NotificationCenter
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         NotificationCenter.default.addObserver(self, selector: #selector(orientationChanged), name: UIDevice.orientationDidChangeNotification, object: nil)
@@ -51,26 +71,20 @@ class MainViewController: UIViewController {
         super.viewDidDisappear(animated)
         NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
     }
-    @objc func orientationChanged () {
+    
+    @objc private func orientationChanged () {
         if UIDevice.current.orientation.isPortrait {
-            swipeToShare.text = "Swipe up to share"
+            swipeToShareLabel.text = "Swipe up to share"
         } else {
-            swipeToShare.text = "Swipe left to share"
+            swipeToShareLabel.text = "Swipe left to share"
         }
     }
-    // Check the orientation of the phone
-    func landscape () -> Bool {
-        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft ||
-            UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
-            return true
-        }
-        return false
-    }
-    @objc func swipeToShare(_ gesture: UISwipeGestureRecognizer) {
+    
+    @objc private func swipeToShare(_ gesture: UISwipeGestureRecognizer) {
         selectActionButton()
         guard viewImageVerification() else  {
             let alert = UIAlertController(title: "Operation Impossible", message: "You did not fill all the images", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
             present(alert, animated: true)
             return
         }
@@ -85,24 +99,33 @@ class MainViewController: UIViewController {
             print("unknow direction")
         }
         // Animation that pushes the main view out of the screen
-        UIView.animate(withDuration: 0.3, animations: {
-            self.mainView.transform = transform
-        // Once the animation is finished share the images
-        },completion: {_ in self.shareImage() })
+        UIView.animate(
+            withDuration: 0.3,
+            animations: {
+                self.mainView.transform = transform
+            },
+            // Once the animation is finished share the images
+            completion: { _ in
+                self.shareImage()
+            }
+        )
     }
-       //CheckImagesAreFilled
-       func viewImageVerification() -> Bool {
-           var result = false
-           if buttonActionLeft.isSelected {
-               result = buttonTopRightTapped && buttonTopLeftTapped && buttonBottomRightTapped
-           } else if buttonActionCenter.isSelected {
-               result = buttonTopRightTapped && buttonTopLeftTapped && buttonBottomRightTapped
-           } else if buttonActionRight.isSelected {
-               result = buttonTopRightTapped && buttonTopLeftTapped && buttonBottomRightTapped && buttonBottomLeftTapped
-           }
-           return result
-       }
+    
+    //CheckImagesAreFilled
+   private func viewImageVerification() -> Bool {
+        var result = false
+        if buttonActionLeft.isSelected {
+            result = buttonTopRightTapped && buttonBottomLeftTapped && buttonBottomRightTapped
+        } else if buttonActionCenter.isSelected {
+            result = buttonTopRightTapped && buttonTopLeftTapped && buttonBottomRightTapped
+        } else if buttonActionRight.isSelected {
+            result = buttonTopRightTapped && buttonTopLeftTapped && buttonBottomRightTapped && buttonBottomLeftTapped
+        }
+        return result
+    }
+    
     // MARK: - Select a layout
+    
     // Check that an action button is selected
     private func selectActionButton() {
         guard !buttonActionLeft.isSelected && !buttonActionCenter.isSelected && !buttonActionRight.isSelected else { return }
@@ -111,9 +134,10 @@ class MainViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
+    
     // ActionButtonTapped
-    @IBAction func layoutButtonTapped(_ sender: UIButton) {
-        for button in layoutButtons {
+    @IBAction private func layoutButtonTapped(_ sender: UIButton) {
+        for button in buttonImageArray {
             button.isSelected = false
         }
         sender.isSelected = true
@@ -138,8 +162,10 @@ class MainViewController: UIViewController {
             break
         }
     }
+    
     // MARK: - Share images
-    func shareImage() {
+    
+   private func shareImage() {
         guard let imageToshare = generateImageFromMainView(input: mainView) else {
             print("can not generate image from main view")
             return
@@ -148,21 +174,30 @@ class MainViewController: UIViewController {
         activityController.completionWithItemsHandler = {
             (activityType, completed, returnedItems, error)
             in
-            print("Share Completed")
+            if completed {
+                self.resetElements()
+            } else {
+                self.mainView.transform = .identity
+            }
+            if let shareError = error {
+                print("error while sharing: \(shareError.localizedDescription)")
+            }
         }
         present(activityController, animated: true)
-        start()
     }
+    
     // Transform the image buttons into a single image
-    func generateImageFromMainView(input : UIView) -> UIImage? {
+   private func generateImageFromMainView(input : UIView) -> UIImage? {
         UIGraphicsBeginImageContext(input.frame.size)
         guard let context = UIGraphicsGetCurrentContext() else { return nil }
         input.layer.render(in: context)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         return image
     }
+    
     // MARK: - Retrieve an image from the photo library
-    @IBAction func imageButtonTapped(_ sender: UIButton) {
+    
+    @IBAction private func imageButtonTapped(_ sender: UIButton) {
         switch sender {
         case buttonImageTopLeft:
             buttonTopLeftTapped = true
@@ -182,42 +217,30 @@ class MainViewController: UIViewController {
         imagePickerController.allowsEditing = true
         present(imagePickerController,animated: true)
     }
+    
     // MARK: - Return to the original screen
-    func start() {
-        let originalImage = UIImage(named: "Plus")
+    
+   private func resetElements() {
         if  buttonActionLeft.isSelected {
-            buttonImageTopLeft.setImage(originalImage, for: .normal)
-            buttonImageBottomLeft.setImage(originalImage, for: .normal)
-            buttonImageBottomRight.setImage(originalImage, for: .normal)
             resetImageButtons()
             resetActionButtons()
             mainView.transform = .identity
         } else if buttonActionCenter.isSelected {
-            buttonImageTopRight.setImage(originalImage, for: .normal)
-            buttonImageTopLeft.setImage(originalImage, for: .normal)
-            buttonImageBottomRight.setImage(originalImage, for: .normal)
             resetImageButtons()
             resetActionButtons()
             mainView.transform = .identity
         } else if buttonActionRight.isSelected {
-            buttonImageTopLeft.setImage(originalImage, for: .normal)
-            buttonImageTopRight.setImage(originalImage, for: .normal)
-            buttonImageBottomLeft.setImage(originalImage, for: .normal)
-            buttonImageBottomRight.setImage(originalImage, for: .normal)
             resetImageButtons()
             resetActionButtons()
             mainView.transform = .identity
         } else {
-            buttonImageTopLeft.setImage(originalImage, for: .normal)
-            buttonImageTopRight.setImage(originalImage, for: .normal)
-            buttonImageBottomLeft.setImage(originalImage, for: .normal)
-            buttonImageBottomRight.setImage(originalImage, for: .normal)
             resetImageButtons()
             resetActionButtons()
             mainView.transform = .identity
         }
     }
-    func resetImageButtons() {
+    
+    private func resetImageButtons() {
         let originalImage = UIImage(named: "Plus")
         buttonImageTopLeft.setImage(originalImage, for: .normal)
         buttonImageTopRight.setImage(originalImage, for: .normal)
@@ -225,30 +248,23 @@ class MainViewController: UIViewController {
         buttonImageBottomRight.setImage(originalImage, for: .normal)
         mainView.transform = .identity
     }
-    func resetActionButtons() {
+    
+   private func resetActionButtons() {
         buttonActionLeft.isSelected = false
         buttonActionCenter.isSelected = false
-        buttonActionRight.isSelected = false
+        buttonActionRight.isSelected = true
     }
 }
+
 // MARK: - UIImagePickerController delegate Methode
+
 // Organization of UIImagePicker and delegate method methods
 extension MainViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let originalPicture = info[.originalImage] as? UIImage else { return }
+        
         // According to the button select fill the corresponding image
-        switch buttonTapped {
-        case buttonImageTopLeft:
-            buttonImageTopLeft.setImage(originalPicture, for: .normal)
-        case buttonImageTopRight:
-            buttonImageTopRight.setImage(originalPicture, for: .normal)
-        case buttonImageBottomLeft:
-            buttonImageBottomLeft.setImage(originalPicture, for: .normal)
-        case buttonImageBottomRight:
-            buttonImageBottomRight.setImage(originalPicture, for: .normal)
-        default:
-            break
-        }
+        buttonTapped?.setImage(originalPicture, for: .normal)
         buttonTapped = nil
         picker.dismiss(animated: true, completion: nil)
     }
